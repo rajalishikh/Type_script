0986081e2656675540bf249a8805d8e09a264779
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-06-06 12:40:13 +0600

    Now I am using spread operator in object

--- src/spread.ts
+++ src/spread.ts
@@ -2,4 +2,14 @@ let number1: number[] = [1, 23, 4, 6];
 let number2: number[] = [4, 7, 9, 111, 3333];
 
 let my_total_Number = [...number1, ...number2];
-console.log(my_total_Number);
+
+const user: {
+  name: string;
+  age: string;
+} = {
+  name: "Raj",
+  age: "26",
+};
+
+const addNewMember = { ...user, role: "Teacher" };
+console.log(addNewMember);
