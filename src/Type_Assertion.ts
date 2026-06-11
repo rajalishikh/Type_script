d6222ff2d6123eb50827f69acd6fe31146761b2e
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-06-11 11:55:57 +0600

    Now I am learn about type assertion

---
+++ src/Type_Assertion.ts
@@ -0,0 +1,20 @@
+let anyThing: any = 1235;
+anyThing as number;
+
+// make a function kg to gram converter
+
+const kg_to_gramConverter = (
+  input: string | number,
+): string | number | undefined => {
+  if (typeof input === "number") {
+    return input * 10000;
+  } else if (typeof input === "string") {
+    const [value] = input.split(" ");
+    return `converted string into number ${Number(value) * 1000}`;
+  }
+};
+
+const number = kg_to_gramConverter(2) as number;
+console.log({ number });
+const number2 = kg_to_gramConverter("2 kg") as string;
+console.log({ number2 });
